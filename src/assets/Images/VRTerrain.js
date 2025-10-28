5f0902f8df0b7e991ba2fb2949dc1ff61b6fd65f
Udeshya Kumar <57803843+Udeshya30@users.noreply.github.com>
2025-10-28 10:40:26 +0530

    Fix image file extension for vrTerrain5 import

--- src/assets/Images/VRTerrain.js
+++ src/assets/Images/VRTerrain.js
@@ -1,4 +1,4 @@
 import vrTerrain4 from "../../assets/Images/VRTerrainVisualization/4.png";
-import vrTerrain5 from "../../assets/Images/VRTerrainVisualization/5.PNG";
+import vrTerrain5 from "../../assets/Images/VRTerrainVisualization/5.png";
 
 export default [vrTerrain4, vrTerrain5];
