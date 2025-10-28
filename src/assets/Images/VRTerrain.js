c07ff1e3b8df9b8c132a7e6c282cbf6c68659457
Udeshya Kumar <57803843+Udeshya30@users.noreply.github.com>
2025-10-28 10:30:54 +0530

    Fix import paths for VR terrain images

--- src/assets/Images/VRTerrain.js
+++ src/assets/Images/VRTerrain.js
@@ -1,4 +1,4 @@
-import vrTerrain4 from "../../assets/Images/VR Terrain Visualization/4.png";
-import vrTerrain5 from "../../assets/Images/VR Terrain Visualization/5.PNG";
+import vrTerrain4 from "../../assets/Images/VRTerrainVisualization/4.png";
+import vrTerrain5 from "../../assets/Images/VRTerrainVisualization/5.PNG";
 
 export default [vrTerrain4, vrTerrain5];
